faea28dee8fd2b6eac7f633cc2bc32b626a3cc42
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-06-02 16:16:42 -0300

    Update App.java
    Removendo escrita em local errado

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-60
+
         int cep;
 
         Scanner cepIn = new Scanner(System.in);
